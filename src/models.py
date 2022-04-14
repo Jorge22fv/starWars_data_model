a355d143680bc6ca3a112934863c62e6b8a840dc
Jorge22fv <jorge.22fv@gmail.com>
2022-04-14 22:37:46 +0000

    tabla fav agregada

--- src/models.py
+++ src/models.py
@@ -8,30 +8,40 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    user_name = Column(String(250), nullable=False)
-    user_email = Column(String(100), nullable=False)
-
-class Character(Base):
-    __tablename__ = 'character'
-    id = Column(Integer, primary_key=True)
-    character_name = Column(String(250), nullable=False)
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     planet_name = Column(String(250))
-    character_id = Column(Integer, ForeignKey('character.id'))
-    character = relationship(Character)
-
+   
 class Starship(Base):
     __tablename__ = 'starship'
     id = Column(Integer, primary_key=True)
     starship_name = Column(String(250))
+   
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    starship_id = Column( Integer, ForeignKey('starship.id'))
+    planet = relationship(Planet)
+    starship = relationship(Starship)
+
+class Fav_character(Base):
+    __tablename__ = 'fav_character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
     character_id = Column(Integer, ForeignKey('character.id'))
     character = relationship(Character)
+    
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    user_email = Column(String(100), nullable=False)
+    fav_char = Column(Integer, ForeignKey('fav_character.id'))
+    character =  relationship(Fav_character)
 
     def to_dict(self):
         return {}
